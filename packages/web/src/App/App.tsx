f6af5b9ef9871313bc62e9bf2b97594ea681e8f5
Vaibhav <vaibhavagarwal220@gmail.com>
2021-02-06 23:00:27 +0530

    css and lint fixes

--- packages/web/src/App/App.tsx
+++ packages/web/src/App/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AppCSS from './App.module.css';
 import People from '../People/People';
 import Shows from '../Shows/Shows'
