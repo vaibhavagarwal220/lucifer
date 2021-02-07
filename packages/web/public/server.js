cead729066b0a7ea202fe57ca77b7419552d9683
Vaibhav <vaibhavagarwal220@gmail.com>
2021-02-07 17:13:30 +0530

    code changes

--- packages/web/public/server.js
+++ packages/web/public/server.js
@@ -4,6 +4,11 @@ var options = {
     index: 'index.html'
 };
 //Catch All Route for URL
-server.use('/*', express.static('/home/site/wwwroot', options));
+server.use('/public', express.static(__dirname, options));
+
+server.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+  
 server.listen(process.env.PORT);
 console.log('Started server on Port', process.env.PORT);
